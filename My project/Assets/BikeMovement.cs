d4be3d9618e902adc30f795409413bf80ca95b74
danww1738 <101942502+danww1738@users.noreply.github.com>
2022-07-18 17:05:22 +0100

    Changed Map and Movement
    Better implemented a map floor and edited movement to include jumping and different inputs

---
+++ My project/Assets/BikeMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float rotationSpeed;
+    public float jumpSpeed;
+
+    private CharacterController cController; // Unity implemented class to deal with movement.
+    private float ySpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal"); // Unity logic to record input as -1 0 or 1 for horizontal movement.
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+       // float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed; // This is for controller input.
+        movementDirection.Normalize(); //Stops the speed exceeding 1 for example diagonol.
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if(cController.isGrounded){
+
+            ySpeed = -0.5f;
+
+            if(Input.GetButtonDown("Jump")){
+                ySpeed = jumpSpeed;
+            }
+        }
+
+        Vector3 velo = movementDirection;
+        velo.y = ySpeed;
+
+        cController.Move(velo * Time.deltaTime);
+
+
+
+
+        //if(movementDirection != Vector3.zero){    //Check if moving
+        //    Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        //}
+        //This is to rotate to direction moving
+
+    }
+
+}
