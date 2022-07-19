133228561092e289d1096581bd7f01552179bf64
danww1738 <101942502+danww1738@users.noreply.github.com>
2022-07-19 17:29:34 +0100

    Camera following movement.
    The camera follows in a 3rd person view and movement is base off direction facing.

--- My project/Assets/BikeMovement.cs
+++ My project/Assets/BikeMovement.cs
@@ -8,6 +8,7 @@ public class BikeMovement : MonoBehaviour
     public float speed;
     public float rotationSpeed;
     public float jumpSpeed;
+    public float gravityStrength;
 
     private CharacterController cController; // Unity implemented class to deal with movement.
     private float ySpeed;
@@ -20,13 +21,23 @@ public class BikeMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); // Unity logic to record input as -1 0 or 1 for horizontal movement.
-        float verticalInput = Input.GetAxis("Vertical");
+        if(transform != null){
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed, 0);
+            var forward = transform.TransformDirection(Vector3.forward);
+            float curSpeed = speed * Input.GetAxis("Vertical");
+            cController.SimpleMove(forward * curSpeed);
+        }
 
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize(); 
 
-        ySpeed += Physics.gravity.y * Time.deltaTime;
+        //float horizontalInput = Input.GetAxis("Horizontal"); // Unity logic to record input as -1 0 or 1 for horizontal movement.
+        //float verticalInput = Input.GetAxis("Vertical");
+
+        // Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+       // movementDirection.Normalize(); 
+
+
+
+        //ySpeed += (Physics.gravity.y * gravityStrength) * Time.deltaTime;
 
         if(cController.isGrounded){
 
@@ -37,10 +48,10 @@ public class BikeMovement : MonoBehaviour
             }
         }
 
-        Vector3 velo = movementDirection*speed;
-        velo.y = ySpeed;
+        // Vector3 velo = movementDirection*speed;
+        //velo.y = ySpeed;
 
-        cController.Move(velo * Time.deltaTime);
+        //cController.Move(velo * Time.deltaTime);
 
 
 
@@ -48,7 +59,7 @@ public class BikeMovement : MonoBehaviour
         //if(movementDirection != Vector3.zero){    //Check if moving
         //    Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
         //    transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        //}
+       // }
         //This is to rotate to direction moving
 
     }
