81d5038c4e1578a25f4a826b8497df569a307c58
danww1738 <101942502+danww1738@users.noreply.github.com>
2022-08-02 23:11:30 +0100

    Replaced Map and added custom objects.
    Created a new map using pro-builder and new objects such as trees and fences. Still major issue with movement needs to be addressed.

--- My project/Assets/Scripts/BikeMovement.cs
+++ My project/Assets/Scripts/BikeMovement.cs
@@ -7,16 +7,21 @@ public class BikeMovement : MonoBehaviour
     private float vInput;
     private float hInput;
     private float frontRotation;
+    private float momentum;
     public float rotationSpeed;
-    public float bikeSpeed;
+    public float bikeAcceleration;
     public GameObject peddle;
     public GameObject front;
     public GameObject frontWheel;
     public GameObject backWheel;
+    public float steeringSensitivity;
+    public float addGravity;               // The game already has gravity. This just increases it on the bike alone.
 
     private void Start()
     {
+        GetComponent<Rigidbody>().AddForce(Physics.gravity * addGravity, ForceMode.Acceleration);
         frontRotation = 0.0f;
+        momentum = 0.0f;
     }
 
     void Update(){
@@ -26,25 +31,46 @@ public class BikeMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(hInput>0 && frontRotation < 90.0f){
-            frontRotation += (hInput + rotationSpeed)*Time.deltaTime;
+        //if(grounded){};
+        if(momentum < 20 && vInput > 0){
+        momentum += vInput * bikeAcceleration;
         }
 
-        if(hInput<0 && frontRotation > -90.0f){
-            frontRotation -= (hInput + rotationSpeed)*Time.deltaTime;
+        if(vInput == 0){
+            if(momentum > 0){
+                 momentum -= 1.0f;
+            }
+            if(momentum < 0){
+                momentum = 0;
+            }
+        }
+
+        if(hInput > 0 && frontRotation < 90.0f){
+            frontRotation += (hInput + rotationSpeed) * Time.deltaTime;
+        }
+
+        if(hInput< 0 && frontRotation > -90.0f){
+            frontRotation -= (hInput + rotationSpeed) * Time.deltaTime;
         }
 
         front.transform.localRotation  = Quaternion.Euler(0, frontRotation, 0);
-        //transform.localRotation = Quaternion.Euler(0, 0, (-frontRotation/5));
         
-        transform.Rotate(0, (frontRotation * vInput)/90, 0);
-        transform.Translate(Vector3.forward * vInput * Time.deltaTime * bikeSpeed);
+        transform.Rotate(0, (frontRotation * vInput) / steeringSensitivity, 0);
+        transform.Translate((Vector3.forward * Time.deltaTime * momentum));
 
-        float partRotation = (vInput * Time.deltaTime) * 300;
+        if(vInput > 0){
+            float partRotation = (vInput * Time.deltaTime) * 300;
 
-        peddle.transform.Rotate(partRotation,0,0);
-        frontWheel.transform.Rotate(partRotation,0,0);
-        backWheel.transform.Rotate(partRotation,0,0);
+            peddle.transform.Rotate(partRotation,0,0);
+            frontWheel.transform.Rotate(partRotation,0,0);
+            backWheel.transform.Rotate(partRotation,0,0);
     }
+        }
+
+
+
+   // bool isGrounded(){
+       // return Physics.Raycast(transform.position);
+    //}
 
 }
