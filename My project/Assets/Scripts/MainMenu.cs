02d037c74fb6781e7f22944ce5fd49960afab696
danww1738 <101942502+danww1738@users.noreply.github.com>
2022-08-14 21:25:42 +0100

    Edited Main Menu
    Changed the main menu to make it look better.

--- My project/Assets/Scripts/MainMenu.cs
+++ My project/Assets/Scripts/MainMenu.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    //public void LoadLevelSelect();
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
 
     public void QuitGame()
     {
         Application.Quit();
-        Debug.Log("test"); // Exits ( doesn't work in editor.)
     }
 }
 
